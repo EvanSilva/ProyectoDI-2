53023bc21d73e0b82c041e1befb8acd39ff3412e
Evan Silva <evanchapela@gmail.com>
2024-09-24 09:45:15 +0200

    24/09/2024

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from PIL.ImageQt import qt_version
 
 from venPrincipal import *
 import sys
+import var
 
 class main(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(main, self).__init__()
-        self.ui = Ui_venPrincipal()
-        self.ui.setupUi(self)
+        var.ui = Ui_venPrincipal()
+        var.ui.setupUi(self)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])

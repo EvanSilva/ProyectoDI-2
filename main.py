a505964485c8b5ae0006db78ca223af01199ca7c
Evan Silva <evanchapela@gmail.com>
2024-09-25 11:29:34 +0200

    25/09/2024

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from PIL.ImageQt import qt_version
-
 from venPrincipal import *
 import sys
 import var
 
-class main(QtWidgets.QMainWindow):
-
+class Main(QtWidgets.QMainWindow):
     def __init__(self):
-        super(main, self).__init__()
+        super(Main, self).__init__()
         var.ui = Ui_venPrincipal()
         var.ui.setupUi(self)
 
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
-    window = main()
-    window.show()
+    window = Main()
+    window.showMaximized()
     sys.exit(app.exec())
